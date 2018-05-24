4b91a46047eec8afee0c5b1c008de1dc2ce6cbf4
Yevhen Karychkovskyi <jack.karichkovskiy@gmail.com>
2018-05-24 15:29:52 +0200

    improved theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { TutorialComponent } from './tutorial/tutorial.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 
